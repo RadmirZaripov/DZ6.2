5620ea00109b563aff3ffb088d7bb207212d0820
unknown <kenseisan23@gmail.com>
2023-05-04 01:38:48 +0300

    Gitignore added

---
+++ src/test/java/ru/netology/javaqa/javaqamvn/services/RestServiceTest.java
@@ -0,0 +1,34 @@
+package ru.netology.javaqa.javaqamvn.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class RestServiceTest {
+    @ParameterizedTest
+    //@CsvSource({
+            //"3,10000,3000,20000",
+            //"2,100000,60000,150000"
+
+    //})
+    @CsvFileSource(files = "src/test/resources/Rest.cvs")
+    public void rest(int expected, int income, int expenses, int threshold) {
+        RestService service = new RestService();
+        //int expected = 3;
+        int actual = service.calculate (income,expenses,threshold);
+        Assertions.assertEquals(expected, actual);
+        }
+
+        //@Test
+       // public void rest2() {
+            //RestService service = new RestService();
+        //int expected = 2;
+          //int actual = service.calculate (100000 , 60000,150000 );
+          //Assertions.assertEquals(expected, actual);
+
+       // }
+
+    }
+
